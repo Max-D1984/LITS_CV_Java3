a82a2fdc70b8503e14cd8cb1589dc307d3cc2e24
Max-D1984 <is96071@gmail.com>
2019-11-20 15:49:07 +0200

    Modify Record (add toString, add getter)

--- src/main/java/dto/Record.java
+++ src/main/java/dto/Record.java
@@ -1,4 +1,40 @@
 package dto;
 
+import filter.District;
+
+import java.time.LocalDate;
+
 public class Record {
+
+    private LocalDate yearMonthDay;
+    private District region;
+    private Integer registrationCount;
+
+    public Record(LocalDate yearMonthDay, District region, Integer registrationCount){
+        this.yearMonthDay = yearMonthDay;
+        this.region = region;
+        this.registrationCount = registrationCount;
+    }
+
+    public LocalDate getYearMonthDay() {
+        return yearMonthDay;
+    }
+
+    public District getRegion() {
+        return region;
+    }
+
+    public Integer getRegistrationCount() {
+        return registrationCount;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Record{" +
+                "yearMonthDay=" + yearMonthDay +
+                ", region=" + region +
+                ", registrationCount=" + registrationCount +
+                '}';
+    }
 }
