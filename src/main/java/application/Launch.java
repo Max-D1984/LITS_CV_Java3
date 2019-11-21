8fd3fcb004e7169484b939727de82aaf491268bb
oleh0704@gmail.com <oleh578856>
2019-11-21 20:51:01 +0200

    Add interface in Launcher

--- src/main/java/application/Launch.java
+++ src/main/java/application/Launch.java
@@ -8,10 +8,7 @@ import filter.RecordManager;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Launch {
     private static boolean goOnAsking = true;
@@ -31,37 +28,54 @@ public class Launch {
 
     private static void defineFilters(Scanner in) {
 
-        String first;
+        int first;
+        System.out.println("Your option are: ");
+        System.out.println("1 - filter on Year;");
+        System.out.println("2 - filter on Month;");
+        System.out.println("3 - filter on District;");
+        System.out.println("4 - filter on Quarter.");
         System.out.println("Choose the filter: ");
-        first = in.nextLine();
 
-        switch (first) {
-            case "1":
-                System.out.println("You did select the filter on Year!");
-                System.out.println("Please enter Year:");
-                filterOnYear(in.nextLine());
-                break;
-            case "2":
-                System.out.println("You did select the filter on Month!");
-                System.out.println("Please enter Month:");
-                filterOnMonth(in.nextLine());
-                break;
+        try {
+            first = in.nextInt();
+            switch (first) {
+                case 1:
+                    System.out.println("You did select the filter on Year!");
+                    System.out.println("Please enter Year:");
+                    filterOnYear(in.nextLine());
+                    break;
+                case 2:
+                    System.out.println("You did select the filter on Month!");
+                    System.out.println("Please enter Month:");
+                    filterOnMonth(in.nextLine());
+                    break;
 
-            case "3":
-                System.out.println("You did select the filter on District");
-                System.out.println("Please enter District:");
-                filterOnDistrict(in.nextLine());
-                break;
+                case 3:
+                    System.out.println("You did select the filter on District");
+                    System.out.println("Please enter District:");
+                    filterOnDistrict(in.nextLine());
+                    break;
 
-            case "4":
-                System.out.println("You did select the filter on Quarter");
-                break;
+                case 4:
+                    System.out.println("You did select the filter on Quarter");
+                    break;
 
-            case "0":
-                goOnAsking = false;
-                System.out.println("Thanks for using our program");
-                break;
+                case 0:
+                    goOnAsking = false;
+                    System.out.println("Thanks for using our program");
+                    break;
+                default:
+                    System.out.println("Incorrect number of option!");
+                    System.out.println();
+
+            }
+        }catch(InputMismatchException ex){
+            System.out.println("You need to input numerical value");
+            System.out.println();
         }
+
+
+
 
     }
     private static void filterOnMonth(String enteredMonth) {
@@ -81,6 +95,7 @@ public class Launch {
         Collection<Record> records = recordManager.filterOnYear(localDate);
         recordManager.printRecords(records);
     }
+
     private static void filterOnDistrict(String districtName){
         System.out.println("Selected District " + districtName);
         Collection<Record> records = recordManager.filterOnDistrict(District.getDistrict(districtName));
